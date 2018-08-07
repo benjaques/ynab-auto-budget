50ff2647032b7109806a4737f08ce507d404f41c
benjaques <apps.benj@gmail.com>
2018-08-07 19:30:21 -0400

    added filter for transactions

--- budgeter.py
+++ budgeter.py
@@ -1,4 +1,13 @@
-from datetime import datetime
+"""
+Job computes funds needed to budget all scheduled transactions in YNAB.
+Finds most active budget, cycles through scheduled transactions that live
+within the time period between now and next paycheck, budgets for those
+respective categories, and spits out the info regarding how much money is left
+for that window to spend at will.
+
+Should be run as cron job on the days user is paid.
+"""
+from datetime import datetime, timedelta
 import json
 import requests
 import config
@@ -11,6 +20,8 @@ HEADERS = {'Content-Type': 'application/json', 'Authorization': 'Bearer {0}'.for
 # date stuff
 ISO_FORMAT = '%Y-%m-%dT%H:%M:%S%z'
 EARLIEST_DATE = '1970-01-01T00:00:00+00:00'
+CURRENT_DATE = datetime.now()
+NEXT_INCOME = CURRENT_DATE + timedelta(days=config.PAYMENT_WINDOW)
 
 # returns json object of response given url 
 def get_json_response(endpoint):
@@ -32,12 +43,42 @@ def find_budget_id(json_response):
             if (max(budget_date, most_recent) == budget_date):
                 most_recent = budget_date
                 budget_id = budget['id']
-            return budget_id
-        else:
-            return None
+        return budget_id
+    else:
+        return None
+
+# cycle through transactions, removing any that are not due until next paycheck
+def filter_transactions(scheduled_transactions):
+    filtered_transactions = []
+    for transaction in scheduled_transactions:
+        next_occurrence = datetime.strptime(transaction['date_next'], '%Y-%m-%d')
+        if CURRENT_DATE < next_occurrence < NEXT_INCOME:
+            filtered_transactions.append(transaction)
+
+    return filtered_transactions
+
+def earmark_categories(to_be_budgeted, transactions, categories):
+    for t in transactions:
+        category_name = t['category_name']
+        amount = milli_unit_converter(t['amount'])
+        print(category_name, amount)
 
 
+
+# ynab stores currency as milliunits
+def milli_unit_converter(amount):
+    return abs(amount / 1000)
+
+# find id of active budget
 budgets_response = get_json_response('budgets')
 budget_id = find_budget_id(budgets_response)
+
+# find scheduled transactions and filter by payment window
 scheduled_transactions = get_json_response('budgets/{0}/scheduled_transactions'.format(budget_id))
-print(json.dumps(scheduled_transactions, indent=4))
+immediate_transactions = filter_transactions(scheduled_transactions['data']['scheduled_transactions'])
+
+current_month = get_json_response('budgets/{0}/months/current'.format(budget_id))
+to_be_budgeted = current_month['data']['month']['to_be_budgeted']
+categories = current_month['data']['month']['categories']
+
+earmark_categories(to_be_budgeted, immediate_transactions, categories)
--- exampleconfig.py
+++ exampleconfig.py
@@ -1,3 +1,6 @@
 
 # generate api token from ynab -> account management -> dev settings
 API_TOKEN = "aaaaabbbbbbbcccccccddddddeeeeee"
+
+# payment window, days between each payday
+PAYMENT_WINDOW = 15 
